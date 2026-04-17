aebcd8b8369f477b725d5cd49c5ed7a9ac33e7ee
Aïmène Saoud <aimenesaoud@gmail.com>
2026-04-17 10:10:21 +0200

    update

--- index.js
+++ index.js
@@ -4,14 +4,3 @@ require('dotenv').config();
 
 // Pour que votre serveur puisse lire les formulaires envoyés par React
 app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
-
-// Route de test
-app.get('/', (req, res) => {
-    res.send("Le serveur InternSync est allumé et prêt !");
-});
-
-app.listen(PORT, () => {
-    console.log(`Serveur lancé sur http://localhost:${PORT}`);
-});
